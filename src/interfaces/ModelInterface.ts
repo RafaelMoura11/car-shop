80abb25449ea91e611d2ea60b0497ca86699a174
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-05-30 18:45:41 -0300

    Corrigindo importacao de ModelInterface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,9 +1,6 @@
-interface Model<T> {
+export interface Model<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
   readOne(id_: string): Promise<T | null>,
-  update(id_: string, obj: T): Promise<T | null>, 
+  update(id_: string, obj: T): Promise<T | null>,
 }
-
-// eslint-disable-next-line import/prefer-default-export
-export { Model };
