af3bcaafd55ee6c1680e4df65ba9a944adc78009
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-05-30 21:24:06 -0300

    Corrigindo os tipos de doorsQty e seatsQty

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import VehicleSchema from './VehicleInterface';
 
 const CarSchema = VehicleSchema.extend({
-  doorsQty: z.string().min(2).max(4),
-  seatsQty: z.string().min(2).max(7),
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
 });
 
 type Car = z.infer<typeof CarSchema>;
