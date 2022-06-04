c7d64e60e9e827d7bf3ba858218d5acbea81b033
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-06-04 13:23:01 -0300

    Instanciando car e adicionando rota

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import CustomRouter from './routes/Router';
+import App from './app';
+
+import CarController from './controllers/car';
+
+import { Car } from './interfaces/CarInterface';
+
+const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
+
+export default server;
