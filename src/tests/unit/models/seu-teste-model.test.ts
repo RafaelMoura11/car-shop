6d44de1ad2e581c6af7fb1a0a184fcc3ca9dcbe8
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-06-05 14:54:37 -0300

    Testando func read de carModel

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,2 +1,53 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import * as sinon from 'sinon';
+import CarModel from '../../../models/car';
+import { Types } from 'mongoose';
+
+const arrayOfCars = [
+  {
+    _id: new Types.ObjectId(),
+    model: 'Uno da Escada',
+    year: 1963,
+    color: 'red',
+    buyValue: 3500,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    _id: new Types.ObjectId(),
+    model: 'Ferrari',
+    year: 2004,
+    color: 'black',
+    buyValue: 350000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    _id: new Types.ObjectId(),
+    model: 'Gol',
+    year: 2013,
+    color: 'gray',
+    buyValue: 35000,
+    seatsQty: 4,
+    doorsQty: 4
+  },
+]
+
+const carModel = new CarModel();
+
+describe('Testando model de car', () => {
+  describe('Testando função read', () => {
+    before(() => {
+      sinon.stub(Model, 'find').resolves(arrayOfCars);
+    })
+    after(() => {
+      (Model.find as sinon.SinonStub).restore();
+    })
+
+    it('Verifica retorno', async () => {
+      const cars = await carModel.read();
+      expect(cars).to.be.deep.equal(arrayOfCars);
+    })
+  })
+})
